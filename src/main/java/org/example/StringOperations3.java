b202c78c240669a1c76b649c2c82e43e2d3c6d9b
Pawel Kupinski <pawel.kupinski@sonarsource.com>
2024-12-23 13:52:34 +0100

    Dummy commit

--- src/main/java/org/example/StringOperations3.java
+++ src/main/java/org/example/StringOperations3.java
@@ -10,6 +10,7 @@ public class StringOperations3 {
         System.out.println("Reversed: " + reversed);
         System.out.println("Uppercased: " + uppercased);
         System.out.println("Lowercased: " + lowercased);
+        System.out.println("Eeeeeeeee: " + lowercased);
     }
 
     public static String reverseString(String str) {
